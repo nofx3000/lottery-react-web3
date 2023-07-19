ad2aff2daecff88140d611fa38ff5bdbb7b8f2fc
nofx3000 <398622407@qq.com>
2023-07-19 10:07:11 +0800

    first commit

---
+++ contract/lottery/deploy.js
@@ -0,0 +1,29 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { Web3 } = require("web3");
+const { abi, evm } = require("./compile");
+
+const provider = new HDWalletProvider(
+  "cube treat excuse argue sunset cattle vicious common split mango stable nominee",
+  "https://sepolia.infura.io/v3/d7f33a16143d433187d3065d2a4dbaa7"
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log("Attempting to deploy from account", accounts[0]);
+  const result = await new web3.eth.Contract(abi)
+    .deploy({
+      data: evm.bytecode.object,
+      arguments: [],
+    })
+    .send({
+      from: accounts[0],
+      gas: "1000000",
+    });
+  console.log(JSON.stringify(abi));
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+
+deploy();
--- src/App.js
+++ src/App.js
@@ -1,22 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
+import web3 from "./web3";
+import lottery from "./lottery";
 function App() {
+  const [players, setPlayers] = useState([]);
+  const [manager, setManager] = useState("");
+  const [balance, setBalance] = useState("");
+  const [message, setMessage] = useState("");
+  const [bet, setBet] = useState(0);
+  useEffect(() => {
+    getPlayers();
+    getManager();
+    getBalance();
+  }, []);
+
+  const getPlayers = async () => {
+    const players = await lottery.methods.getPlayers().call();
+    console.log("--", JSON.stringify(players));
+
+    setPlayers(players);
+  };
+
+  const getManager = async () => {
+    const manager = await lottery.methods.manager().call();
+    setManager(manager);
+  };
+
+  const getBalance = async () => {
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    console.log("--", balance);
+    setBalance(balance);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+    setMessage("sending transaction...");
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: web3.utils.toWei(bet, "ether"),
+      });
+      setMessage("transaction is successful!");
+    } catch (err) {
+      setMessage(err.toString());
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <p>manager is {manager}</p>
+        <p>players are {players.length}</p>
+        <p>balance is {web3.utils.fromWei(balance, "ether")} ether</p>
+        <form
+          onSubmit={(e) => {
+            onSubmit(e);
+          }}
         >
-          Learn React
-        </a>
+          <label>input the value you're going to bet</label>
+          <input
+            value={bet}
+            onChange={(e) => {
+              setBet(e.target.value);
+            }}
+          ></input>
+          <div>
+            <button>Enter</button>
+          </div>
+        </form>
+        <p>{message}</p>
       </header>
     </div>
   );
